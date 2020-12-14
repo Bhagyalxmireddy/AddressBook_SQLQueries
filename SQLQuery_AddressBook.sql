6cc8dfb7c970044bb4eb1bcd5f130fd067519654
Bhagi <puppyreddy1997@gmail.com>
2020-12-14 20:26:08 +0530

    [Bhagyalaxmi]ADDED:Creating the AddressBook Table

--- SQLQuery_AddressBook.sql
+++ SQLQuery_AddressBook.sql
@@ -1,3 +1,15 @@
 create Database Address_Book
 use Address_Book
-show Address_Book
+
+CREATE TABLE Address_Book (
+    ID int  IDENTITY(1,1) primary key,
+    FirstName varchar(50),
+	LastName varchar(50),
+	Address varchar(50),
+	City varchar(50),
+	State varchar(50),
+	Zip varchar(6),
+	PhoneNumber varchar(10),
+	Email varchar(50),  
+);
+select * from Address_Book;
